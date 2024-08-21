3ece095b49eb470b0c593bcd45d9df22d2351d63
unknown <1520477@senati.pe>
2024-08-21 12:57:33 -0500

    suma

--- python.py
+++ python.py
@@ -1,1 +1,8 @@
 print ("hola mundo")
+n1 = float(input("Intro número uno: "))
+n2 = float(input("Intro numero dos: "))
+suma = n1+n2
+print("La suma es: ", suma)
+n3 = float(input("la resuma: "))
+l = suma+n3
+print(" la resuma es: ", l)
