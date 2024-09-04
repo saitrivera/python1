6605b57921973c6e8645eb6cdcc56e9ffe201ed2
chrisSanchez00 <1534663@gmail.com>
2024-09-04 09:48:05 -0500

    first commit

--- python.py
+++ python.py
@@ -1,8 +1,6 @@
-print ("hola mundo")
-n1 = float(input("Intro número uno: "))
-n2 = float(input("Intro numero dos: "))
-suma = n1+n2
-print("La suma es: ", suma)
-n3 = float(input("la resuma: "))
-l = suma+n3
-print(" la resuma es: ", l)
+tuple=()
+n1=float(input("Calificacion de matematica:"))
+n2=float(input("Calificacion de comunicacion:"))
+n3=float(input("Calificacion de computacion:"))
+
+print(tuple)
